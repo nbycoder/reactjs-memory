773783862d927399558cadfc7903bc3edfb48e68
ellis <90400844+nbycoder@users.noreply.github.com>
2021-12-21 15:08:34 +0000

    Updates

---
+++ precache-manifest.595407d14f4dad39c2b5e2ce59816491.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "33f14750a3bfdf0aa4b2",
+    "url": "/reactjs-memory/static/js/main.a5c08ebb.chunk.js"
+  },
+  {
+    "revision": "0415fec3622ed2180202",
+    "url": "/reactjs-memory/static/js/runtime~main.adebec68.js"
+  },
+  {
+    "revision": "0c93d5cbd616d6ed065c",
+    "url": "/reactjs-memory/static/js/2.a6919538.chunk.js"
+  },
+  {
+    "revision": "be64ee63614d17a7b9b16cf667291cc1",
+    "url": "/reactjs-memory/index.html"
+  }
+];
